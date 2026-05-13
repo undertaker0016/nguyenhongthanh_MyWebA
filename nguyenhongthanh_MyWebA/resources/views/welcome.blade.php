5db1b74e9d3d4a5c9d7ebbba3e692375779d289b
undertaker0016 <thanhne01225@gmail.com>
2026-05-13 21:22:59 +0700

    up file

---
+++ nguyenhongthanh_MyWebA/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thông Tin Cá Nhân</title>
+</head>
+<body>
+    <h1>THÔNG TIN CÁ NHÂN</h1>
+
+    <p>Họ tên: Nguyễn Hồng Thanh</p>
+    <p>MSSV: 2124110220</p>
+    <p>Lớp: CCQ2411G</p>
+    <p>Email: thanhne01225@gmail.com</p>
+</body>
+</html>
