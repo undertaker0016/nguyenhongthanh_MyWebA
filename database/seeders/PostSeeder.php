9f35786915c79485da973ea1bfae9c50b5309417
undertaker0016 <thanhne01225@gmail.com>
2026-06-02 14:54:29 +0700

    update lab 5D

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+
 class PostSeeder extends Seeder
 {
     /**
@@ -13,17 +13,17 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 1; $i <= 10; $i++) {
+        for ($i = 1; $i <= 50; $i++) {
 
-            $title = fake()->unique()->sentence(6);
+            $title = fake()->sentence(rand(3, 6));
 
             DB::table('posts')->insert([
-                'title' => $title,
-                'slug' => Str::slug($title),
-                'image' => 'post-' . $i . '.png',
-                'status' => fake()->numberBetween(0, 1),
-                'user_id' => rand(1, 3),
-                'content' => fake()->paragraph(3),
+                'title'      => $title,
+                'slug'       => Str::slug($title) . '-' . $i,
+                'content'    => fake()->paragraphs(rand(3, 8), true),
+                'image'      => 'post-' . rand(1, 10) . '.jpg',
+                'status'     => rand(0, 1),
+                'user_id'    => 1, 
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
